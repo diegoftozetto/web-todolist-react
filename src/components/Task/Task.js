431d6865520d63ec966d59037f0c410f988930b9
Diego <diegotozetto@gmail.com>
2020-11-24 20:49:24 -0300

    Add Input e Handler no componente 'Task'

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -1,6 +1,18 @@
+import { NavLink } from "react-router-dom";
+import { useState } from 'react';
 import "./Task.css"
 
 function Task() {
+  const [ taskName, setTaskName ] = useState('');
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+    console.log({
+      taskName
+    })
+  }
+
+
   return (
     <div className="container mt-4">
       <div className="card">
@@ -9,10 +21,10 @@ function Task() {
         </div>
         <div className="card-body">
           <h6>Nome da Tarefa</h6>
-          <form>
-            <input type="text" className="form-control mr-2 mb-2" id="task-name" placeholder="Informe o nome da tarefa..."/>
-            <button type="submit" className="btn btn-secondary mr-2">Cancelar</button>
-            <button type="submit" className="btn btn-primary">Salvar</button>
+          <form onSubmit={formSubmitHandler}>
+            <input autoFocus type="text" className="form-control mb-2" name="taskName" id="taskName" placeholder="Informe o nome da tarefa..." value={taskName} onChange={event => setTaskName(event.target.value)}/>
+            <NavLink className="btn btn-secondary btn-sm mr-2" to="/home" role="button">Cancelar</NavLink>
+            <button type="submit" className="btn btn-primary btn-sm">Salvar</button>
           </form>
         </div>
       </div>
