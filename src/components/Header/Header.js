75dcf014c1ccc66c89b18d2fe9baf768bb182e4c
Diego <diegotozetto@gmail.com>
2020-11-24 01:07:03 -0300

    Ajuste e estilo no componente 'Header'

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,17 @@
+import { Link, NavLink } from "react-router-dom";
 import "./Header.css"
 
 function Header() {
   return (
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <a class="navbar-brand" href="#">To Do List</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
+      <Link className="navbar-brand" to="/home">To Do List</Link>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div class="navbar-nav">
-          <a class="nav-link" href="#">Home</a>
-          <a class="nav-link" href="#">Nova Tarefa</a>
+      <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div className="navbar-nav">
+          <NavLink className="nav-link" to="/home">Home</NavLink>
+          <NavLink className="nav-link" to="/task">Nova Tarefa</NavLink>
         </div>
       </div>
     </nav>
