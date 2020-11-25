e14505ec3eacf7fbd7104e8ffc7f8e96805f1cd2
Diego <diegotozetto@gmail.com>
2020-11-25 10:57:40 -0300

    Modifica o nome da aplicação

--- src/components/ToDoList/ToDoList.js
+++ src/components/ToDoList/ToDoList.js
@@ -56,7 +56,7 @@ function ToDoList() {
   return (
     <div className="container mt-4">
       <div className="jumbotron">
-        <h1 className="display-4">To Do List</h1>
+        <h1 className="display-4">To-Do List</h1>
         <p className="lead">Libere espaço na sua mente. Recupere a clareza e a tranquilidade, tirando todas essas tarefas da sua cabeça e colocando na sua lista de tarefas.</p>
         <hr/>
         <NavLink className="btn btn-primary btn-sm mb-4" to="/task" role="button">+ Nova Tarefa</NavLink>
