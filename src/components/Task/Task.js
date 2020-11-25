4e03ae620a1b7b8e0f4ba9d59cd47995bee0a02c
Diego <diegotozetto@gmail.com>
2020-11-25 14:27:39 -0300

    Retorna a lista de tarefas ao adicionar nova tarefa

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -1,9 +1,11 @@
-import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import { useState } from 'react';
 import "./Task.css"
 
 function Task() {
   const [ taskName, setTaskName ] = useState('');
+  const history = useHistory();
 
   const formSubmitHandler = (event) => {
     event.preventDefault();
@@ -17,7 +19,7 @@ function Task() {
     }).then((res)=> {
       res.json().then(dados => {
         if(res.status === 201) {
-          event.target.taskName.value = "";
+          history.push('/home');
           //console.log(dados.message)
         } else {
           //console.log(dados.message)
@@ -36,7 +38,7 @@ function Task() {
           <h6>Nome da Tarefa</h6>
           <form onSubmit={formSubmitHandler}>
             <input autoFocus type="text" className="form-control mb-2" name="taskName" id="taskName" placeholder="Informe o nome da tarefa..." value={taskName} onChange={event => setTaskName(event.target.value)}/>
-            <NavLink className="btn btn-secondary btn-sm mr-2" to="/home" role="button">Cancelar</NavLink>
+            <Link className="btn btn-secondary btn-sm mr-2" to="/home" role="button">Cancelar</Link>
             <button type="submit" className="btn btn-primary btn-sm">Salvar</button>
           </form>
         </div>
