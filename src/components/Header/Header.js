ec9e99f63c4bff5ddd164191f9205ac576e998a2
Diego <diegotozetto@gmail.com>
2020-11-25 14:43:23 -0300

    Troca do ícone da aplicação

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,6 +4,8 @@ import "./Header.css"
 function Header() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <img src={process.env.PUBLIC_URL + '/logochecklists.png'} width="30" height="30" className="d-inline-block align-top" alt=""/>
+
       <Link className="navbar-brand" to="/home">To-Do List</Link>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
