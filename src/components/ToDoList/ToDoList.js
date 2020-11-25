8d53f76f3e3e6d30e0d7a77c11d69832350b1461
Diego <diegotozetto@gmail.com>
2020-11-25 10:50:05 -0300

    Modificação no carregamento

--- src/components/ToDoList/ToDoList.js
+++ src/components/ToDoList/ToDoList.js
@@ -14,46 +14,44 @@ function ToDoList() {
     });
   }, []);
 
-  if(!tasks)
-    return <div className="container mt-4" id="loading">Carregando...</div>;
+  const loading = <p id="loading">Carregando...</p>;
 
-  const checkMarkedHandler = (event) => {
-    console.log()
-    const id = event.target.id.substring(7);
+  var items, notFoundTasks, list;
+  if(tasks !== undefined) {
+    const checkMarkedHandler = (event) => {
+      const id = event.target.id.substring(7);
+      const json = {marked: event.target.checked}
 
-    const json = {
-      marked: event.target.checked
+      fetch('https://web-api-todolist.herokuapp.com/tasks/' + id, {
+        method: "PUT",
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(json)
+      }).then((res)=> {
+        res.json().then(dados => {
+          //console.log(dados.message)
+        });
+      });
     }
 
-    fetch('https://web-api-todolist.herokuapp.com/tasks/' + id, {
-      method: "PUT",
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(json)
-    }).then((res)=> {
-      res.json().then(dados => {
-        //console.log(dados.message)
-      });
-    });
+    items = tasks.map((task) =>
+      <li className="list-group-item" key={task._id}>
+        <input
+          id={`marked_${task._id}`}
+          type="checkbox"
+          className="mr-2"
+          value={task.marked}
+          onChange={checkMarkedHandler}
+          defaultChecked={task.marked}
+        />
+        <label htmlFor={`marked_${task._id}`}>
+          {task.text}
+        </label>
+      </li>
+    );
+
+    notFoundTasks = <p id="no-task">Nenhuma tarefa encontrada...</p>
+    list = <ul className="list-group list-group-flush">{items}</ul>;
   }
-
-  const items = tasks.map((task) =>
-    <li className="list-group-item" key={task._id}>
-      <input
-        id={`marked_${task._id}`}
-        type="checkbox"
-        className="mr-2"
-        value={task.marked}
-        onChange={checkMarkedHandler}
-        defaultChecked={task.marked}
-      />
-      <label htmlFor={`marked_${task._id}`}>
-        {task.text}
-      </label>
-    </li>
-  );
-
-  const notFoundTasks = <p id="no-task">Nenhuma tarefa encontrada...</p>
-  const list = <ul className="list-group list-group-flush">{items}</ul>;
 
   return (
     <div className="container mt-4">
@@ -62,7 +60,7 @@ function ToDoList() {
         <p className="lead">Libere espaço na sua mente. Recupere a clareza e a tranquilidade, tirando todas essas tarefas da sua cabeça e colocando na sua lista de tarefas.</p>
         <hr/>
         <NavLink className="btn btn-primary btn-sm mb-4" to="/task" role="button">+ Nova Tarefa</NavLink>
-        {items.length === 0 ? notFoundTasks : list}
+        {tasks ? (items.length === 0 ? notFoundTasks : list) : loading}
       </div>
     </div>
   );
